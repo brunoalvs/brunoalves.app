a1c71aef00260cac5ea533ba8bf0b27327cac83c
Bruno Alves <hello.brunoalves@gmail.com>
2021-12-12 06:12:07 +0000

    Last adjustment related to the styled components configuration and fonts style

--- pages/styles.tsx
+++ pages/styles.tsx
@@ -1,6 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-display: swap;
+    font-family: 'Fira Sans Condensed';
+    font-weight: 300;
+    src: url('/fonts/FiraSansCondensed-Light.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Fira Sans Condensed';
+    font-weight: normal;
+    src: url('/fonts/FiraSansCondensed-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'Fira Sans Condensed';
+    font-weight: bold;
+    src: url('/fonts/FiraSansCondensed-Bold.woff2') format('woff2');
+  }
 
   *, *:before, *:after {
     box-sizing: inherit;
@@ -8,8 +28,11 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     box-sizing: border-box;
-    scroll-behavior: smooth;
+    font-family: 'Fira Sans Condensed', sans-serif;
+    font-display: swap;
+    font-size: 16px;
     text-rendering: optimizeLegibility;
+    scroll-behavior: smooth;
 
     @media (prefers-reduced-motion: reduce) {
       scroll-behavior: auto;
@@ -27,12 +50,20 @@ export const GlobalStyle = createGlobalStyle`
 
 
   [data-theme="dark"] {
-    --background-layout: #1b1732;
+    --background-layout: linear-gradient(171.9deg, #1B1732 0%, #0A0813 100%);;
+    --background-topheader: ##000000;
+    --background-navmenu: linear-gradient(171.9deg, #0A0813 0%, #000000 100%);;
     --text-color: #fcfcfc;
+    --primary-color: #8368EE;
+    --secondary-color:  ##68EEB6;
   }
 
   [data-theme="light"] {
     --background-layout: #ecedf3;
+    --background-topheader: #FBFBFC;
+    --background-navmenu: #F6F6FC;
     --text-color: #040716;
+    --primary-color: #8368EE;
+    --secondary-color:  #35C789;
   }
 `;
