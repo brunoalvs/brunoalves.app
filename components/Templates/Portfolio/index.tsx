c3978d3e0d0c5905135f3070a8bf6dd795103654
Bruno Alves <hello.brunoalves@gmail.com>
2021-12-18 01:49:48 +0000

    Organizando a casa

---
+++ components/Templates/Portfolio/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TemplatePortfolio: React.FC = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default TemplatePortfolio;
---
+++ pages/portfolio.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TemplatePortfolio from "../components/Templates/Portfolio";
+
+const Portfolio: React.FC = () => {
+  return (
+    <TemplatePortfolio>
+      <h1>Portfolio</h1>
+    </TemplatePortfolio>
+  );
+};
+
+export default Portfolio;
