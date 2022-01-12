4739886f73e708c729ee1cee1d2307e5b719a6eb
Bruno Alves <hello@brunoalves.app>
2022-01-12 22:55:43 +0000

    styles improves in skills and contact pages

--- styles/page.about.tsx
+++ styles/page.about.tsx
@@ -1,7 +1,16 @@
 import styled from "styled-components"
 
 export const Container = styled.section`
-  .teste {
-    border: 1px dashed hotpink;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  gap: 1.5rem;
+
+  > h1 {
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    gap: 2rem;
   }
 `
--- styles/page.skills.tsx
+++ styles/page.skills.tsx
@@ -2,6 +2,13 @@ import styled from "styled-components"
 
 export const Container = styled.section`
   width: 100%;
+  height: 100%;
+  display: grid;
+  gap: 1.5rem;
+
+  > h1 {
+    margin-bottom: 1rem;
+  }
 
   > .grid {
     display: grid;
@@ -26,5 +33,9 @@ export const Container = styled.section`
         font-size: 1.6rem;
       }
     }
+  }
+
+  @media (min-width: 768px) {
+    gap: 2rem;
   }
 `
