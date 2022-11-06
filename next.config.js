9177429e718cc9bbf06f80fc07960a0ce16f1e6f
Bruno Alves <bruno.d@tamanna.com>
2022-11-06 11:28:40 +0000

    markdown removed

--- next.config.js
+++ next.config.js
@@ -7,25 +7,6 @@ module.exports = {
       "avatars.githubusercontent.com",
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        use: "raw-loader",
-      },
-      {
-        test: /\.mdx$/,
-        use: [
-          {
-            loader: "@mdx-js/loader",
-            /** @type {import('@mdx-js/loader').Options} */
-            options: {},
-          },
-        ],
-      }
-    )
-    return config
-  },
   reactStrictMode: true,
   i18n: {
     locales: ["en", "pt"],
